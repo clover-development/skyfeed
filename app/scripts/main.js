7588c2877e172bc4f9a3592e23de0736486a5576
Vladislav Galushko <lordjocker6300@gmail.com>
2017-04-06 17:06:13 +0300

    the first but not last

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,28 @@
-const electron = require('electron')
+const electron = require('electron');
 // Require and activate debug tools
 require('electron-debug')({showDevTools: false});
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 //const protocol = electron.protocol
 // Dependencies
 
 const pug = require('electron-pug')({pretty: true});
-const path = require('path')
-const window = require('electron-window')
+const path = require('path');
+const window = require('electron-window');
 
 function createWindow () {
   // Create the browser window.
-  const mainWindow = window.createWindow({ width: 800, height: 600, show: true  })
-  const indexPath = path.join(__dirname, '../index.pug')
-  mainWindow.showUrl(indexPath)
+
+  const mainWindow = window.createWindow({ width: 800, height: 600, show: true  });
+  const indexPath = path.join(__dirname, '../index.pug');
+  mainWindow.showUrl(indexPath);
+  mainWindow.openDevTools();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -29,7 +31,7 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
@@ -37,7 +39,7 @@ app.on('activate', function () {
   if (mainWindow === null) {
     createWindow()
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- app/scripts/renderer.js
+++ app/scripts/renderer.js
@@ -1,3 +1,43 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// const {BrowserWindow} = require('electron');
+// let win = new BrowserWindow({width: 800, height: 600});
+// win.on('closed', () => {
+//   win = null
+// });
+
+angular.module('skyFeed', [])
+  .controller("LoginCtrl", function ($scope, $http) {
+    $scope.loginFb = function () {
+      $http({
+        method: 'GET',
+        url: 'https://www.facebook.com/v2.8/dialog/oauth',
+        params: {
+          client_id: 1722090841416106,
+          redirect_uri: 'https://www.facebook.com/connect/login_success.html',
+          scope: ['user_actions.news', ''],
+          display: 'popup',
+          response_type: 'token'
+        }
+      }).then(function (res) {
+      }, function () {
+        alert('error');
+      })
+    };
+
+    $scope.loginVk = function () {
+      $http({
+        method: 'GET',
+        url: 'https://oauth.vk.com/authorize',
+        params: {
+          client_id: '5959809',
+          redirect_uri: 'https://oauth.vk.com/blank.html',
+          display: 'page',
+          scope: 'wall, messages, notifications, notify',
+          response_type: 'token'
+        }
+      }).then(function (res) {
+        // win.loadURL(`res.data`);
+      }, function (res) {
+        alert('error, ' + res);
+      });
+    }
+  });
