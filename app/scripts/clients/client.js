a070f167e8015ddf096deee9ea0539d53714e8b9
Alexander Khomchenko <alexandr.khomchenko@gmail.com>
2017-05-07 21:32:09 +0300

    move common methods to parent class

--- app/scripts/clients/client.js
+++ app/scripts/clients/client.js
@@ -1,6 +1,35 @@
+const randomColor = require('randomcolor');
+
 class Client {
+    constructor(args) {
+        if (!args.token) {
+            throw new Error('Token is required');
+        }
+        this.token = args.token;
+        this.color = args.color || randomColor();
+        this.active = true;
+    }
+
     // This function fetches the posts from users news feed
     getPosts(page = 0, callback) { }
+
+    getAttributes() {
+        return {
+            token: this.token,
+            color: this.color,
+            klass: this.constructor.name
+        }
+    }
+
+    htmlColor() {
+        return {
+            'color': this.color
+        };
+    }
+
+    switchActive() {
+        this.active = !this.active;
+    }
 }
 
 module.exports = Client;
--- app/scripts/clients/vk-client.js
+++ app/scripts/clients/vk-client.js
@@ -1,37 +1,17 @@
 const VKApi = require('node-vkapi');
-const randomColor = require('randomcolor');
 const Client = require('./client');
 
 class VKClient extends Client {
     constructor(args) {
-        super();
-        if (!args.token) {
-            throw new Error('Token is required');
-        }
-        this.token = args.token;
+        super(args);
         this.apiClient = new VKApi({ token : args.token });
         this.type = 'vk';
-        this.color = args.color || randomColor();
     }
 
     getPosts(page = 0, callback) {
         this.apiClient.call('newsfeed.get', {}).then(res => {
             callback(res.items);
         });
-    }
-
-    getAttributes() {
-        return {
-            token: this.token,
-            color: this.color,
-            klass: this.constructor.name
-        }
-    }
-
-    htmlColor() {
-        return {
-            'color': this.color
-        };
     }
 }
 
