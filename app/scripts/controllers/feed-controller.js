34bad750b63d2dcbff43889f44f31a70c4cf2007
Alexander Khomchenko <alexandr.khomchenko@gmail.com>
2017-05-11 22:43:04 +0300

    update posts fetch code

--- app/scripts/controllers/feed-controller.js
+++ app/scripts/controllers/feed-controller.js
@@ -7,29 +7,13 @@ skyfeed.controller('FeedController', function ($scope, $state) {
 
     $scope.load = function () {
         let logins = loginService.getLogins();
-        console.log(logins);
         if (!logins.length) { return $state.go('app.login'); }
         logins.forEach(function (login) {
             $scope.fetching = true;
             login.getPosts(0, posts => {
-                $scope.items = posts;
-                $scope.fetching = false;
-                $scope.$apply();
-                console.log('Get Posts Result:\n', posts);
-            });
-        });
-    };
-
-    $scope.loadMoreItems = function () {
-        let logins = loginService.getLogins();
-        logins.forEach(function (login) {
-            $scope.fetching = true;
-            $scope.$apply();
-            login.getPosts(0, posts => {
                 $scope.items.push.apply($scope.items, posts);
                 $scope.fetching = false;
                 $scope.$apply();
-                console.log('Push new posts:\n', posts);
             });
         });
     };
