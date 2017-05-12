4e7758a25fd6af53e0306c9ce9c64cfe8c0b1746
Alexander Khomchenko <alexandr.khomchenko@gmail.com>
2017-05-12 17:45:43 +0300

    use 4 spaces as tab in main

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,25 +16,25 @@ let mainWindow = undefined;
 console.log('Application settings path is: ', app.getPath('userData'));
 
 function createWindow () {
-  const iconPath = path.join(__dirname, '../images/icon.png');
-  mainWindow = new BrowserWindow({ width: 1800, height: 1000, icon: iconPath });
-  const indexPath = path.join(__dirname, '../index.jade');
-  const indexUrl = url.format({ protocol: 'file', pathname: indexPath, slashes: true});
-  mainWindow.loadURL(indexUrl);
-  mainWindow.openDevTools();
-  mainWindow.webContents.on('did-finish-load', () => { mainWindow.show() });
+    const iconPath = path.join(__dirname, '../images/icon.png');
+    mainWindow = new BrowserWindow({ width: 1800, height: 1000, icon: iconPath });
+    const indexPath = path.join(__dirname, '../index.jade');
+    const indexUrl = url.format({ protocol: 'file', pathname: indexPath, slashes: true});
+    mainWindow.loadURL(indexUrl);
+    mainWindow.openDevTools();
+    mainWindow.webContents.on('did-finish-load', () => { mainWindow.show() });
 }
 
 app.on('ready', createWindow);
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 });
 app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow()
-  }
+    if (mainWindow === null) {
+        createWindow()
+    }
 });
 
 ipcMain.on('vk-button-clicked', function () {
